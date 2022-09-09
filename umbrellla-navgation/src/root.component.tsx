47d7cbbd826ffa5cfe51a5b2f98b1d8a7e838971
André <cazetto.andre@gmail.com>
2022-09-09 07:24:54 -0300

    feat: add a simple top navbar using Force Components Design System

--- umbrellla-navgation/src/root.component.tsx
+++ umbrellla-navgation/src/root.component.tsx
@@ -1,3 +1,34 @@
+import React from 'react';
+import {
+  themePrimer,
+  PrimerGlobalStyle,
+  ForceThemeProvider,
+  Box,
+  Button,
+  Text,
+} from 'force-components';
+
+const GlobalStyle = <PrimerGlobalStyle />;
+
 export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+  return (
+    <ForceThemeProvider theme={themePrimer} globalStyle={GlobalStyle}>
+      <Box
+        display="flex"
+        bg="colorGray200"
+        height="50vh"
+        alignItems="center"
+        justifyContent="center"
+        flexDirection="row"
+        px="4"
+      >
+        <Button variant="primary" mx="2" as="a" href="/">
+          Shop
+        </Button>
+        <Button variant="secondary" mx="2" as="a" href="/checkout">
+          Payment
+        </Button>
+      </Box>
+    </ForceThemeProvider>
+  );
 }
